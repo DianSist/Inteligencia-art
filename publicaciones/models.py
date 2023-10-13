0b37b990dbf21b4878e08ccec01f3ea0ee01f5f7
diansist <leo_electro_21@hotmail.com>
2023-10-13 10:43:16 -0600

    Ejercicio01

---
+++ publicaciones/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Posts(models.Model):
+    title = models.CharField(verbose_name= 'Título de la publicación', max_length=150)
+    content = models.TextField(verbose_name='Agrega el contenido de la publicación')
+    status = models.SmallIntegerField(verbose_name='Indica el estado de la publicación')
+    publishedDate = models.DateTimeField('Especificar fecha de publicación')
+
+    
+    def _str_(self):
+        return self.title 
+    
+    class Meta:
+        verbose_name = 'Publicación'
+        verbose_name_plural = 'Publicaciones'
